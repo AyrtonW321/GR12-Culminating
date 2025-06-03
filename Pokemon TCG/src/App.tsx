722a2ea2b008768a019d351df3938b10ccad4cb8
Ayrton Wong <[ayrtonwongg@gmail.com]>
2025-06-03 14:37:50 -0400

    Set up files

--- Pokemon TCG/src/App.tsx
+++ Pokemon TCG/src/App.tsx
@@ -1,13 +1,18 @@
-
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   // Settings
+  
 
   return (
-    <>
-{/* CODE goes here */}
-    </>
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/battle" element={<Battle />} />
+      </Routes>
+    </Router>
   )
 }
 
